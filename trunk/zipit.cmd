6fd2424f16f68d3f51a6877b41626f70014f7a4e
zorkzero@hotmail.com <zorkzero@hotmail.com@a2a190f2-9f3f-0410-b117-8d365baceae5>
2015-04-18 20:16:35 +0000

    v2.12

--- trunk/zipit.cmd
+++ trunk/zipit.cmd
@@ -1,5 +1,5 @@
 setlocal
-set VERSION=2.11
+set VERSION=2.12
 "%ProgramFiles%\7-zip\7z.exe" u furigana-inserter-%VERSION%.xpi -tzip -r components content defaults locale mecab skin chrome.manifest COPYING.GPL install.rdf license.txt -x!*.user -x!*.ncb -x!*.suo -x!Release -x!content\tests
 "%ProgramFiles%\7-zip\7z.exe" t furigana-inserter-%VERSION%.xpi
 pause
